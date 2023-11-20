5b9574813e7e33456190dd553886b26e6a5be537
Administrator <2933310375@qq.com>
2023-11-20 18:26:21 +0800

    123

--- Examination/StudentExamination/middleware/auth.py
+++ Examination/StudentExamination/middleware/auth.py
@@ -1,14 +1,18 @@
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
-
+import re
 
 class LoginSeesion(MiddlewareMixin):
     """校验登录的session是否相同"""
 
     def process_request(self, request):
-        if request.path_info in ["/login/", "/api/gtimg/", "/register/",
-                                 "/api/test/","/api/testlogin/","/api/testmain/",
-                                 "/api/getxx/",]:
+        try:
+            re_d = re.compile(r"\/api\/.?").findall(request.path_info)
+            print(re_d)
+        except Exception as e:
+            print(str(e))
+            re_d = None
+        if request.path_info in ["/login/", "/register/",] and re_d:
             pass
         else:
             if request.session.get("info"):
