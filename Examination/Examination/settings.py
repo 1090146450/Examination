45c47f5da704dcb83e1ae227d38a94e816b02f18
vm <vm.@git>
2023-12-12 21:35:51 +0800

    123

--- Examination/Examination/settings.py
+++ Examination/Examination/settings.py
@@ -108,100 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-# 日志
-<<<<<<< HEAD
-# cur_path = os.path.dirname(os.path.realpath(__file__))  # log_path是存放日志的路径
-# log_path = os.path.join(os.path.dirname(cur_path), 'logs')
-# if not os.path.exists(log_path): os.mkdir(log_path)  # 如果不存在这个logs文件夹，就自动创建一个
-#
-# LOGGING = {
-#     'version': 1,
-#     # 'disable_existing_loggers': True,  # 禁用已经存在的logger实例
-#     # 日志文件格式
-#     'formatters': {
-#         # 详细的日志格式
-#         'standard': {
-#             'format': '[%(asctime)s] [%(filename)s:%(lineno)d] [%(module)s:%(funcName)s] '
-#                       '[%(levelname)s]- %(message)s'},
-#         # 简单的日志格式
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         },
-#     },
-#     # 过滤器
-#     'filters': {
-#
-#     },
-#     # 定义具体处理日志的方式
-#     'handlers': {
-#         # 默认记录所有日志
-#         'default': {
-#             'level': 'INFO',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(log_path, 'all-{}.log'.format(time.strftime('%Y-%m-%d'))),
-#             'maxBytes': 1024 * 1024 * 50,  # 文件大小
-#             'backupCount': 5,  # 备份数
-#             'formatter': 'standard',  # 输出格式
-#             'encoding': 'utf-8',  # 设置默认编码，否则打印出来汉字乱码
-#         },
-#         # 输出错误日志
-#         'error': {
-#             'level': 'ERROR',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(log_path, 'error-{}.log'.format(time.strftime('%Y-%m-%d'))),
-#             'maxBytes': 1024 * 1024 * 50,  # 文件大小
-#             'backupCount': 5,  # 备份数
-#             'formatter': 'standard',  # 输出格式
-#             'encoding': 'utf-8',  # 设置默认编码
-#         },
-#         # 告警日志输出
-#         'WARNING': {
-#             'level': 'WARNING',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': os.path.join(log_path, 'WARNING-{}.log'.format(time.strftime('%Y-%m-%d'))),
-#             'maxBytes': 1024 * 1024 * 50,  # 文件大小
-#             'backupCount': 5,  # 备份数
-#             'formatter': 'standard',  # 输出格式
-#             'encoding': 'utf-8',  # 设置默认编码
-#         },
-#         # # 控制台输出
-#         # 'console': {
-#         #     'level': 'DEBUG',
-#         #     'class': 'logging.StreamHandler',
-#         #     'formatter': 'standard'
-#         # },
-#         # # 输出info日志
-#         # 'info': {
-#         #     'level': 'INFO',
-#         #     'class': 'logging.handlers.RotatingFileHandler',
-#         #     'filename': os.path.join(log_path, 'info-{}.log'.format(time.strftime('%Y-%m-%d'))),
-#         #     'maxBytes': 1024 * 1024 * 50,
-#         #     'backupCount': 5,
-#         #     'formatter': 'standard',
-#         #     'encoding': 'utf-8',  # 设置默认编码
-#         # },
-#     },
-#     # 配置用哪几种 handlers 来处理日志
-#     'loggers': {
-#         # 类型 为 django 处理所有类型的日志， 默认调用
-#         'django': {
-#             'handlers': ['default', 'error', 'WARNING'],
-#             # 'handlers': ['default', 'console', 'WARNING'],
-#             # , 'console'
-#             'level': 'INFO',
-#             'propagate': False  # 向不向更高级别的logger传递
-#         },
-#     },
-#     # log 调用时需要当作参数传入
-#     'log': {
-#         'handlers': ['error', 'WARNING', 'default'],
-#         # 'handlers': ['error', 'info', 'WARNING', 'console', 'default'],
-#         # 'error', 'info', 'console',
-#         'level': 'INFO',
-#         'propagate': True
-#     },
-# }
-=======
 cur_path = os.path.dirname(os.path.realpath(__file__))  # log_path是存放日志的路径
 log_path = os.path.join(os.path.dirname(cur_path), 'logs')
 if not os.path.exists(log_path): os.mkdir(log_path)  # 如果不存在这个logs文件夹，就自动创建一个
@@ -293,7 +199,7 @@ LOGGING = {
         'propagate': True
     },
 }
->>>>>>> 661b388a535d042c99717921d50f38799ec35f90
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
